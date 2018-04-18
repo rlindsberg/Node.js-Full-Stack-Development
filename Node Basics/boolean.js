3436532b2a94a53988b8b5fbc58ae4c03390d2aa
rlindsberg <roderick@karlemstrand.com>
2018-04-18 08:24:40 +0200

    Add function to toogle a boolean variable

--- Node Basics/boolean.js
+++ Node Basics/boolean.js
@@ -1,5 +1,19 @@
 var isValid = false;
 
-if (isValid === false) {
-  console.log('Data is not valid!');
+function isVarValid(booleanVar){
+  if (booleanVar) {
+    console.log('Data is valid!');
+  } else if (booleanVar === false) {
+    console.log('Data is not valid!');
+  }
 }
+
+function toogleBool (booleanVar){
+  if (typeof booleanVar === 'boolean') {
+    return !booleanVar;
+  }
+}
+
+isVarValid(isValid);
+var isValid = toogleBool(isValid);
+isVarValid(isValid);

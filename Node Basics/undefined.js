7b2072ec9abf5a3f997e1d77c8c2f8100c22bc30
rlindsberg <roderick@karlemstrand.com>
2018-04-23 10:53:07 +0200

    Updated underfined

--- Node Basics/undefined.js
+++ Node Basics/undefined.js
@@ -1,2 +1,31 @@
-var nameö
-console.log(name;);
+var name;
+console.log(name);
+
+function doesNothing(){
+
+}
+
+console.log(doesNothing());
+
+console.log('How to use undefined in an if statement:');
+name = undefined;
+if (name === undefined) {
+  console.log('name is undefined');
+}
+
+console.log('How to use typeof:');
+if (typeof x === 'undefined') {
+  console.log('x if undefined');
+}
+
+function greetUser(myname){
+  if (typeof myname === 'undefined') {
+    console.log('Hello stranger!');
+  } else {
+    console.log('Hello ' + myname + '!');
+  }
+}
+
+var xyz;
+greetUser('Linn');
+greetUser(xyz);

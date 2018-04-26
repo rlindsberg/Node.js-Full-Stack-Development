bb6ef51433c1216a277b67df9090805918fc83d3
rlindsberg <roderick@karlemstrand.com>
2018-04-26 21:39:49 +0200

    push, unshift, pop and shift

--- Node Basics/array.js
+++ Node Basics/array.js
@@ -1,5 +1,11 @@
 var grade = [90, 80, 'Pass', true];
 
 grade.push(100);
-
+grade.unshift(10);
 console.log(grade);
+
+var gradePop = grade.pop(); //keep the last item
+console.log(gradePop);
+
+var gradePop = grade.shift(); //keep the first item
+console.log(gradePop);

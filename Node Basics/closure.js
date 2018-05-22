aa10af4f929910bc53b9db99a4e3308ded7264fd
rlindsberg <roderick@karlemstrand.com>
2018-05-22 21:38:20 +0200

    Implentent closure in a function to add two numbers

--- Node Basics/closure.js
+++ Node Basics/closure.js
@@ -7,3 +7,16 @@ function greetMaker(name){
 
 var greetRodde = greetMaker('Rodde');
 greetRodde();
+
+
+function createAdder(baseNumber){
+  function add(numberToAdd){
+    return baseNumber + numberToAdd;
+  }
+  return add;
+}
+
+var addTen = createAdder(10);
+console.log(addTen(2));
+console.log(addTen(0));
+console.log(addTen(-2));

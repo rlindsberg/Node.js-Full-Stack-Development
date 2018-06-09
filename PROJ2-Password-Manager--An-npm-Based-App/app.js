cabd41df2aa3aff51f8cf7991230de9cfb426b6b
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-06-09 19:36:12 +0200

    Downgrade node-persist to 0.0.8 to use initSync()

--- PROJ2-Password-Manager--An-npm-Based-App/app.js
+++ PROJ2-Password-Manager--An-npm-Based-App/app.js
@@ -1,1 +1,8 @@
 console.log('Starting application');
+
+//include third party modules using require
+var storage = require('node-persist');
+storage.initSync();
+
+storage.setItemSync('name', 'Andre');
+console.log(storage.getItemSync('name'));

8e96fa0c5280e2eda7760b48380043c28a161473
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-06-16 09:45:06 +0200

    Add alias for command

--- PROJ2-Password-Manager--An-npm-Based-App/example-args.js
+++ PROJ2-Password-Manager--An-npm-Based-App/example-args.js
@@ -2,8 +2,16 @@ var argv = require('yargs')
   .command('hello', 'Greets the user', function(yargs){
     yargs.options({
       name: {
-        demand: true
+        demand: true,
+        alias: 'n' //alias for --name
       }
     }); //.options takes an object as parameter
   }) //command, description, function
   .argv; //chaining
+
+var command = argv._[0];
+console.log(argv);
+
+if (argv.name === 'Neo') {
+  console.log('Do you wanna take a pill?');
+} else console.log('Hello World');

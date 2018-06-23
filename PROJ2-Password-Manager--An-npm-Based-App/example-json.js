f522d7cc23628ab941256e7604900074cb234051
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-06-23 15:50:00 +0200

    convert json back to an object

--- PROJ2-Password-Manager--An-npm-Based-App/example-json.js
+++ PROJ2-Password-Manager--An-npm-Based-App/example-json.js
@@ -7,3 +7,9 @@ var personJSON = JSON.stringify(person);
 
 console.log(personJSON);
 console.log("personJSON's type is " + typeof personJSON);
+
+//convert json back to an object
+var personObject = JSON.parse(personJSON);
+console.log(personObject.name);
+console.log(personObject.age);
+console.log("personObject's type is " + typeof(personObject));

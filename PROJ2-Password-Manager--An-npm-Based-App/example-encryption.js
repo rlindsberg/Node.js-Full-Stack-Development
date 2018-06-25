df24ee862fd31b4b22b86c25477167cb6e1b9d18
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-06-25 19:26:26 +0200

    Decrypt a message

--- PROJ2-Password-Manager--An-npm-Based-App/example-encryption.js
+++ PROJ2-Password-Manager--An-npm-Based-App/example-encryption.js
@@ -3,6 +3,12 @@ var crypto = require('crypto-js');
 var secretMessage = "I hid the crisps under the couch! Don't tell anyone!";
 var secretKey = 'bad-key';
 
+//encrypt mes
 var encryptedMessage = crypto.AES.encrypt(secretMessage, secretKey);
-
 console.log('Encrypted message is: ' + encryptedMessage);
+
+//decrypt mes
+var decryptedBytes = crypto.AES.decrypt(encryptedMessage, secretKey);
+var decryptedMessage = decryptedBytes.toString(crypto.enc.Utf8);
+
+console.log("Decrypted message is : " + decryptedMessage);

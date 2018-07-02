46b4c308e4202c568e7dde63a7e75f9526740231
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-07-02 10:46:23 +0200

    *** Optimize code

--- PROJ2-Password-Manager--An-npm-Based-App/example-encryption.js
+++ PROJ2-Password-Manager--An-npm-Based-App/example-encryption.js
@@ -9,18 +9,18 @@ var secretObj = {
 }
 var secretKey = 'ej0RT-WEO0TÇ7&/8713jkJK.D]-1';
 
-var secretJSON = JSON.stringify(secretObj)
-var encryptedJSON = crypto.AES.encrypt(secretJSON, secretKey);
+
+var encryptedJSON = crypto.AES.encrypt(JSON.stringify(secretObj), secretKey);
 console.log('Encrypted JSON is :' + encryptedJSON);
 
 
 //decrypt an object
 var bytes = crypto.AES.decrypt(encryptedJSON, secretKey);
-var decryptedJSON = bytes.toString(crypto.enc.Utf8);
 
-console.log(decryptedJSON);
-console.log(typeof(decryptedJSON));
 
-var decryptedObj = JSON.parse(decryptedJSON);
+
+
+
+var decryptedObj = JSON.parse(bytes.toString(crypto.enc.Utf8));
 console.log('decrypted Object is: ' + decryptedObj);
 console.log(decryptedObj);

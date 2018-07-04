54d23281423aa89201b03540e6c72d2dd4623eeb
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-07-04 14:27:54 +0200

    Ex. log error message

--- PROJ2-Password-Manager--An-npm-Based-App/example-error-handling.js
+++ PROJ2-Password-Manager--An-npm-Based-App/example-error-handling.js
@@ -1,1 +1,9 @@
-throw new Error('Unable to decrypt account');
+try {
+  throw new Error('Unable to decrypt account');
+} catch (e) {
+  console.log(e.message);
+} finally {
+  console.log('finally');
+}
+
+console.log('try catch ended');

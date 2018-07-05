b20a7fa53c53630153f666f3358438783a16a3fb
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-07-05 23:51:15 +0200

    Use module.exports to include mult. files in a simple app

--- PROJ3-Asynchronous-Weather-App/index.js
+++ PROJ3-Asynchronous-Weather-App/index.js
@@ -1,19 +1,24 @@
 request = require('request');
 var urlAPI = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,SE&units=metric&appid=12f270ae25c272675257d4de97f545de';
+var weather = require('./weather.js');
 
-//takes two parameters
-request({
-  url: urlAPI,
-  json: true
-}, function(error, respons, body){
-  if (error) {
-    console.log('Unable to fetch weather.');
-  } else {  //if no error
-    console.log(JSON.stringify(body, null, 4) + '\n');
+weather();
 
-    var city = body.name;
-    var temp = body.main.temp;
-    console.log('The current temperature in ' + city + ' is ' + temp + '°C.');
-  }
-
-})
+// //takes two parameters
+// request({
+//   url: urlAPI,
+//   json: true
+// }, function(error, respons, body){
+//   if (error) {
+//     console.log('Unable to fetch weather.');
+//   } else {  //if no error
+//     console.log(JSON.stringify(body, null, 4) + '\n');
+//
+//     var city = body.name;
+//     var temp = body.main.temp;
+//     console.log('The current temperature in ' + city + ' is ' + temp + '°C.');
+//   }
+//
+// })
+//
+// console.log('After request!');

3397a9a8c5e9a72129f2539f9c84d7d43128f07e
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-07-09 09:27:29 +0200

    Finish challenge

--- PROJ3-Asynchronous-Weather-App/index.js
+++ PROJ3-Asynchronous-Weather-App/index.js
@@ -3,7 +3,7 @@ var location = require('./location.js');
 var argv = require('yargs')
   .options({
     location:{
-      demand: true,
+      demand: false,
       alias: 'l',
       description: 'Enter your city to get weather',
       type: 'string'
@@ -11,26 +11,24 @@ var argv = require('yargs')
   })
   .argv;
 
-// get weather
+// if location is provided
 if (argv.location != null) {
   console.log(argv.location);
+  // get weather
   weather(argv.l, function(currentWeather) {
     console.log(currentWeather);
   });
+} else { //if location not provided
+  location(function(userLocation) {
+    if (userLocation.errno != null) { //if error
+      console.log('Error requesting ' + userLocation.hostname + ':' + userLocation.port + ' \n');
+      return;
+    }
+    //no error
+    // get weather
+    var city = userLocation.city;
+    weather(city, function(currentWeather) {
+      console.log(currentWeather);
+    });
+  })
 }
-
-
-
-//
-//
-// location(function(userLocation) {
-//   if (userLocation.errno != null) {
-//     console.log('Error requesting ' + userLocation.hostname + ':' + userLocation.port + ' \n');
-//
-//     console.log(userLocation);
-//
-//     return;
-//   }
-//   console.log('city: ' + userLocation.city);
-//   console.log('log/lat: ' + userLocation.loc);
-// })

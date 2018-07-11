9408211d3c7820b43652bf12eba62a72fb7c198a
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-07-11 11:02:15 +0200

    Implement doWork with promises

--- PROJ3-Asynchronous-Weather-App/example-promise.js
+++ PROJ3-Asynchronous-Weather-App/example-promise.js
@@ -8,6 +8,23 @@ function doWork(data, callback) {
 
 }
 
-doWork('some data', function(someOtherData){
+doWork('some data', function(someOtherData) {
   console.log(someOtherData);
 })
+
+//promises
+function doWorkPromise(data) {
+  return new Promise(function(resolve, reject) {
+    if (data === 'some data') {
+      resolve('Work is done!')
+    } else if (data === 'no data') {
+      reject({'error': 'Work error!'}) //return an object, so that multiple attr. can be returned
+    }
+  });
+}
+
+doWorkPromise('no data').then(function(data) {
+  console.log(data);
+}, function (error) {
+  console.log(error);
+}); //first resolve, second reject

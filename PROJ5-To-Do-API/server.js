4ed9a55fc4f4ad5bc652c281d5e5545cdf1bd192
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-07-21 11:36:53 +0200

    - Create todo demo list - Add route to send json

--- PROJ5-To-Do-API/server.js
+++ PROJ5-To-Do-API/server.js
@@ -2,9 +2,27 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000; //heroku port or local port 3000
 
+var todos = [{
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed : false
+  }, {
+    id: 2,
+    description: 'Go to market',
+    completed: false
+  }
+];
+
 app.get('/', function(req, res) {
   res.send('To do API root');
 });
+
+//GET /todos
+app.get('/todos', function(req, res) {
+  res.json(todos);
+});
+
+//GET /todos/<:id>
 
 app.listen(PORT, function() {
   console.log('Express server listening at: 127.0.0.1:' + PORT);

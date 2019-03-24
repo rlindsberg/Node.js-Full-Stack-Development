09ebed67bdcc2daeeb8d603c02f5983798edd590
rlindsberg <roderick@karlemstrand.com>
2019-03-24 22:03:40 +0100

    search the first char from sample in "long", output Found a matched char

--- Internship-Assignment/kattis-probB.js
+++ Internship-Assignment/kattis-probB.js
@@ -7,7 +7,25 @@ const rl = readline.createInterface({
 
 rl.on('line', (line) => {
     var inputFromConsole = line.split(' ');
+    //testing... sample == AGCT
+    //and long == AGCTAGCT
     var sample = inputFromConsole[0];
     var long = inputFromConsole[1]
-    // console.log();
+    console.log(sample.length);
+    matchSample(sample, long, 0, 0)
 });
+
+// i should be 0 from caller
+function matchSample(sample, long, s_index, l_index) {
+    for (; l_index < long.length; l_index++) {
+        //take a char from sample and loop in long
+        //the inner loop quit if no matched char found
+        console.log("The value of l_index is " + l_index + "\n");
+        if (sample[s_index] == long [l_index]) {
+            //found a match
+            console.log("Found a matched char: " + sample[s_index] + "\n");
+            return;
+        }
+    }
+
+}

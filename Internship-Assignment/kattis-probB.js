c7d78c0368d774c4d2ae407f67794d1e3a045610
rlindsberg <roderick@karlemstrand.com>
2019-03-24 21:35:37 +0100

    draft of b

---
+++ Internship-Assignment/kattis-probB.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (line) => {
+    var inputFromConsole = line.split(' ');
+    var sample = inputFromConsole[0];
+    var long = inputFromConsole[1]
+    // console.log();
+});

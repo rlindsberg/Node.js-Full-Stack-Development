b1f6c79b02064a8c6f4e3fdecdc904c2393bc8a1
rlindsberg <roderick@karlemstrand.com>
2019-03-24 22:33:09 +0100

    successfully loop "long" and find "sample"

--- Internship-Assignment/kattis-probB.js
+++ Internship-Assignment/kattis-probB.js
@@ -12,20 +12,45 @@ rl.on('line', (line) => {
     var sample = inputFromConsole[0];
     var long = inputFromConsole[1]
     console.log(sample.length);
-    matchSample(sample, long, 0, 0)
+    console.log(matchSample(sample, long, 0, 0, 0));
 });
 
 // i should be 0 from caller
-function matchSample(sample, long, s_index, l_index) {
+function matchSample(sample, long, s_index, l_index, matchedPrev) {
+    var res = 0;
     for (; l_index < long.length; l_index++) {
         //take a char from sample and loop in long
         //the inner loop quit if no matched char found
         console.log("The value of l_index is " + l_index + "\n");
+
         if (sample[s_index] == long [l_index]) {
             //found a match
+            matchedPrev = 1;
             console.log("Found a matched char: " + sample[s_index] + "\n");
-            return;
+
+            //return sucess if reached the end of sample
+            if (s_index == sample.length - 1) {
+                res++;
+                s_index = 0;
+                console.log("breaking..");
+                break;
+            }
+
+            //then check the next char
+            if (matchSample(sample, long, s_index+1, l_index+1, matchedPrev)) {
+                return 1
+            } else {
+                return 0
+            }
+        } else if (matchedPrev == 1) {
+            console.log("failed, reset s_index");
+            s_index = 0;
+            matchedPrev = 0;
+        } else {
+            console.log("nothing found, moving on into the next char in long");
         }
     }
+    //nothing found
+    console.log(0);
 
 }

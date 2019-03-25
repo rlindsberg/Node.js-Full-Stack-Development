ccbdbde0967134f9dfbe91ab9875b3461d3dd3bf
rlindsberg <roderick@karlemstrand.com>
2019-03-25 12:39:45 +0100

    pre-release, almost done

--- Internship-Assignment/kattisB.js
+++ Internship-Assignment/kattisB.js
@@ -5,8 +5,8 @@ var long = "AGCTAGCT"
 // var long = "AAAAAAAAAA"
 var res = 0;
 // console.log(matchSample(sample, long, long.length));
-console.log(matchReducedString(sample, long, long.length));
-
+// console.log(matchReducedString(sample, long, long.length));
+matchAddedString(sample, long, long.length);
 // i should be 0 from caller
 function matchSample(sample, long, l_index) {
     if (l_index==0)
@@ -40,3 +40,58 @@ function matchReducedString(sample, long, l_index) {
     }
     return res;
 }
+
+//type 3
+function matchAddedString(sample, long, l_index) {
+    var nucleobase = ["A", "C", "G", "T"];
+    var res = [];
+    var res_index = 0;
+    for (var i = 0; i < sample.length; i++) {
+        for (var j = 0; j < 4; j++) {
+            var newString = sample.slice(0, i) + nucleobase[j] + sample.slice(i);
+            for (var k = 0; k < res.length; k++) {
+                if (res[k] == newString) {
+                    break;
+                }
+            }
+            res[res_index] = newString;
+            res_index++;
+        }
+    }
+    // sample.slice(0, 1) + "bar" + txt1.slice(1);
+    console.log("array:");
+    for (var i = 0; i < res.length; i++) {
+        console.log(res[i]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//eof
